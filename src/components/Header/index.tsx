f19af16e7040114f733dd0dbe18a16ef038cf6bd
Daniel Felix <felixdaniel9@gmail.com>
2023-08-30 17:25:04 -0300

    Add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
-
+import logoImg from '../../assets/logo.svg';
+import {FiUser, FiLogIn} from 'react-icons/fi';  
+import { Link } from 'react-router-dom';
 
 export function Header(){
+  const signed = true;
+  const loadingAuth = false;
+
   return(
-    <h1>COMPONENTE HEADER</h1>
+    <div className='w-full flex items-center justify-center bg-white h-16 mb-4 drop-shadow'>
+      < header className='flex w-full mas-x-7xl mx-auto items-center justify-between px-4'>
+        <Link to={'/'}>
+          <img src={logoImg} alt="Logo do site" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to={'/dashboard'}>
+            <div className='border-2 border-black rounded-full p-1'>
+              <FiUser size={22} color="#000"/>
+            </div>
+          </Link>
+        )}
+        {!loadingAuth && !signed && (
+          <Link to={'/login'}>
+            <div className="border-2 border-black rounded-full p-1">
+              <FiLogIn size={22} color="#000"/>
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
   )
 }
