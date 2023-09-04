6734dc6295db9fa7d1522065e4a90644b66fe526
Daniel Felix <felixdaniel9@gmail.com>
2023-09-04 11:05:47 -0300

    Creating context API

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,66 @@
+import {ReactNode, createContext, useState, useEffect} from 'react';
+import {onAuthStateChanged} from 'firebase/auth';
+import { auth } from '../services/firebaseConnection';
+
+interface IAuthProviderProps {
+  children: ReactNode;
+}
+
+type AuthContextDate = {
+  signed: Boolean;
+  loadingAuth: Boolean;
+}
+
+interface IUserProps {
+  uid: string;
+  name: string | null;
+  email: string | null;
+}
+
+export const AuthContext = createContext({} as AuthContextDate)
+
+function AuthProvider({children}: IAuthProviderProps){
+  const [user, setUser] = useState<IUserProps | null>(null)
+  const [loadingAuth, setLoadingAuth] = useState(true);
+
+  // Login
+  useEffect(() => {
+
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if(user){
+        // Tem user logado
+        setUser({
+          uid: user.uid,
+          name: user?.displayName,
+          email: user?.email
+        })
+
+        setLoadingAuth(false);
+      }
+      else{
+        // Não tem user logado
+        setUser(null);
+        setLoadingAuth(false);
+      }
+    })
+
+    // Aqui cancela o olheiro, a verificação que fica olhando se tem user logado.
+    return () => {
+      unsub();
+    }
+
+  }, [])
+
+  return(
+    <AuthContext.Provider
+    value={{
+      signed: !!null,
+      loadingAuth,
+    }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider;
