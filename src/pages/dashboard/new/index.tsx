a4348c17c3d4731cf11635cbb341a8b3dfd02a2b
Daniel Felix <felixdaniel9@gmail.com>
2023-09-05 17:01:02 -0300

    Initializing the car registration page

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,9 +1,27 @@
+import { FiUpload } from "react-icons/fi";
+import { Container } from "../../../components/container";
+import { DashboardHeader } from "../../../components/dashboardHeader";
 
 
 export function New(){
   return(
-    <>
-      <h1>NewCar Page</h1>
-    </>
+    <Container>
+      <DashboardHeader/>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+        <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+          <div className="absolute cursor-pointer">
+            <FiUpload size={30} color='#000'/>
+          </div>
+          <div className="cursor-pointer">
+            <input className="opacity-0 cursor-pointer" type="file" accept="image/*"/>
+          </div>
+        </button>
+      </div>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+
+      </div>
+    </Container>
   )
 }
