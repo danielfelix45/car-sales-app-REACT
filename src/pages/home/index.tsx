5920aed94a212537c0e72b17ec47527d804e9470
Daniel Felix <felixdaniel9@gmail.com>
2023-09-06 19:06:32 -0300

    Listing registered cars

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,67 @@
+import { useState, useEffect } from "react";
+
 import { Container } from "../../components/container";
 
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface ICarsProps{
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string;
+  city: string;
+  km: string;
+  images: ICarImageProps[];
+}
+
+interface ICarImageProps{
+  name: string;
+  uid: string;
+  url: string;
+}
+
 export function Home(){
+  const [cars, setCars] = useState<ICarsProps[]>([]);
+
+  useEffect(() => {
+
+    function loadCars(){
+      // Aqui busca a coleção de carros no banco de dados
+      const carsRef = collection(db, 'cars')
+      // Aqui mostra a lista ordenada
+      const queryRef = query(carsRef, orderBy('created', 'desc'))
+
+      // Aqui busca os itens e mostra os items
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listcars = [] as ICarsProps[];
+
+        snapshot.forEach( doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          })
+        })
+
+        setCars(listcars);
+      })
+
+
+
+    }
+
+    loadCars();
+  }, [])
+
   return(
     <Container>
       <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -15,27 +76,31 @@ export function Home(){
       </h1>
 
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full bg-white rounded-lg">
-          <img 
-            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202308/20230822/volvo-xc60-2.0-t5-momentum-gasolina-4p-automatico-wmimagem11414142937.jpg?s=fill&w=1920&h=1440&q=75" 
-            alt="Carro" 
-          />
-          <p className="font-bold mt-1 mb-2 px-2">VOLVO XC60</p>
+        {cars.map( car => (
+          <Link key={car.id} to={`/car/${car.id}`}>
+            <section className="w-full bg-white rounded-lg">
+              <img 
+                className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+                src={car.images[0].url} 
+                alt="Carro" 
+              />
+              <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-          <div className="flex flex-col px-2">
-            <span className="text-zinc-700 mb-6">Ano 2022/2023 | 80.000 km</span>
-            <strong className="text-black font-medium text-xl">R$ 85.000</strong>
-          </div>
+              <div className="flex flex-col px-2">
+                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+                <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+              </div>
 
-          <div className="w-full h-px bg-slate-300 my-2"></div>
+              <div className="w-full h-px bg-slate-300 my-2"></div>
 
-          <div className="px-2 pb-2">
-            <span className="text-black">
-              Florianopolis - SC
-            </span>
-          </div>
-        </section>
+              <div className="px-2 pb-2">
+                <span className="text-black">
+                  {car.city}
+                </span>
+              </div>
+            </section>          
+          </Link>
+        ))}
       </main>
     </Container>
   )
